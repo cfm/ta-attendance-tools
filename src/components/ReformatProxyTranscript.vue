1fa8553077f739c47cbed0d0858cd1ba7f3aabc1
Cory Francis Myers <cfm@panix.com>
2021-02-16 15:03:47 -0800

    feat: ReformatProxyTranscript form

---
+++ src/components/ReformatProxyTranscript.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-form>
+        <v-textarea></v-textarea>
+        <v-btn>Reformat</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'ReformatProxyTranscript',
+
+  }
+</script>
