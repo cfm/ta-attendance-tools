d8ee5977d2ba1d64e447bc531feb57579b94136f
Cory Francis Myers <cfm@panix.com>
2021-02-17 16:35:00 -0800

    feat: port proxy-transcript reformatter from Jupyter notebook to Vue.js component (#1)
    * build: "vue create -b"
    
    * build: "vue add vuetify"
    
    * feat: ReformatProxyTranscript form
    
    * build: "netlify functions:create"
    
    * build: "netlify link"
    
    * feat(ReformatProxyTranscript): POST to Netlify function
    
    * feat(ReformatProxyTranscript): parse reactively, client-side
    
    * chore: remove default Vue assets
    
    * chore: remove Jupyter notebook

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <ReformatProxyTranscript />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ReformatProxyTranscript from "./components/ReformatProxyTranscript";
+
+export default {
+  name: "App",
+
+  components: {
+    ReformatProxyTranscript,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
