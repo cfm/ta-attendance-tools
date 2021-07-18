bbf38aaddf4536f423b79595046fb26979de8f01
Cory Francis Myers <cfm@panix.com>
2021-07-18 16:25:21 -0700

    feat: load roster and proxy preferences from Salesforce (#21)
    * feat: OAuth against Salesforce
    
    * feat: block on Salesforce SELECT query
    
    * feat: alert on Salesforce query error
    
    * feat: run SELECT query equivalent to exported "proxies.csv"
    
    * feat(TakeAttendance): display roster from Salesforce records

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <v-app>
     <v-main>
+      <Login v-if="loginRequired" />
+      <Loading v-if="syncInProgress" />
+      <v-snackbar v-model="showConnectedToSalesforce" color="success">
+        Connected to Salesforce
+      </v-snackbar>
+      <v-snackbar v-model="showSyncError" color="error">
+        {{ lastSyncError }}
+      </v-snackbar>
       <v-app-bar>
+        <v-chip @click="sync()">
+          <v-avatar><v-icon>mdi-table-refresh</v-icon></v-avatar>
+          <template v-if="lastSync">
+            Roster updated&nbsp;
+            <timeago :datetime="lastSync" />
+          </template>
+          <template v-else> Refresh </template>
+        </v-chip>
         <v-chip :ripple="false">
           <v-avatar>{{ members.length }}</v-avatar> members
         </v-chip>
@@ -15,15 +31,33 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import jsforce from 'jsforce';
+import { mapMutations, mapState } from 'vuex';
 
-import Wizard from "./components/Wizard";
+import { FIELDS } from '@/constants';
+import Loading from './components/Loading';
+import Login from './components/Login';
+import Wizard from './components/Wizard';
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
+    Loading,
+    Login,
     Wizard,
+  },
+
+  data() {
+    return {
+      conn: null,
+      lastSync: null,
+      lastSyncError: false,
+      syncInProgress: false,
+
+      showConnectedToSalesforce: false,
+      showSyncError: false,
+    };
   },
 
   computed: {
@@ -31,6 +65,53 @@ export default {
       members: (state) => state.memberList,
       present: (state) => state.presentList,
     }),
+
+    loginRequired: function () {
+      return this.conn == null;
+    },
+  },
+
+  watch: {
+    conn: function (conn) {
+      if (conn) this.showConnectedToSalesforce = true;
+    },
+    lastSyncError: function () {
+      this.showSyncError = true;
+    },
+  },
+
+  async mounted() {
+    jsforce.browser.init({
+      clientId: process.env.VUE_APP_SALESFORCE_CLIENT_ID,
+      clientSecret: process.env.VUE_APP_SALESFORCE_CLIENT_SECRET,
+      redirectUri: process.env.VUE_APP_SALESFORCE_REDIRECT_URI,
+    });
+    jsforce.browser.on('connect', (conn) => {
+      console.log('Connected to Salesforce');
+      this.conn = conn;
+      this.sync();
+    });
+  },
+
+  methods: {
+    ...mapMutations(['replaceMemberList']),
+    sync() {
+      this.syncInProgress = true;
+      this.conn.query(
+        `SELECT ${FIELDS.join(
+          ', ',
+        )} FROM Contact WHERE Is_Current_TA_Member__c = true`,
+        (err, res) => {
+          this.syncInProgress = false;
+          if (err) {
+            this.lastSyncError = err;
+            return;
+          }
+          this.lastSync = Date.now();
+          this.replaceMemberList(res.records);
+        },
+      );
+    },
   },
 };
 </script>
--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,22 @@
 const maxProxies = 10;
 const proxyKeys = [];
 for (let i = 1; i <= maxProxies; i++) {
-    const proxyKey = `proxy${i}`;
-    proxyKeys.push(proxyKey);
+  const proxyKey = { csv: `proxy${i}`, sf: `Proxy_${i}__c` };
+  proxyKeys.push(proxyKey);
 }
 
 const keys = [
-    "lastName",
-    "firstName",
-    "padding",
-    "lastModified",
-    ...proxyKeys,
+  { csv: 'lastName', sf: 'LastName' },
+  { csv: 'firstName', sf: 'FirstName' },
+  { csv: 'padding', sf: 'Id' },
+  { csv: 'lastModified', sf: 'LastModifiedDate' },
+  ...proxyKeys,
 ];
 
 module.exports = {
-    KEYS: keys,
-    PROXY_KEYS: proxyKeys,
+  KEYS: keys.map((k) => k.csv),
+  PROXY_KEYS: proxyKeys.map((k) => k.csv),
+
+  FIELDS: keys.map((k) => k.sf),
+  PROXY_FIELDS: proxyKeys.map((k) => k.sf),
 };
